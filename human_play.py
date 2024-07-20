2f78ec2a8b5c800c0987d1156405ecd25384ee10
Sunshine <twq566256@foxmail.com>
2024-07-20 22:32:16 +1000

    chore: Update human_play.py with author information and creation date

--- human_play.py
+++ human_play.py
@@ -1,20 +1,11 @@
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-human VS AI models
-Input your move in the format: 2,3
-
-@author: Junxiao Song
-"""
-
-
+# Written by: Sunshine
+# Created on: 20/Jul/2024  22:31
 from env import Env, Game
 from MCTS import MCTSPlayer as MCTS_Pure
 from MCTS_AZ import AlphaZeroPlayer as MCTSPlayer
 from Network import PolicyValueNet
-# from policy_value_net import PolicyValueNet  # Theano and Lasagne
-# from policy_value_net_pytorch import PolicyValueNet  # Pytorch
-# from policy_value_net_tensorflow import PolicyValueNet # Tensorflow
-# from policy_value_net_keras import PolicyValueNet  # Keras
 
 
 class Human(object):
@@ -31,7 +22,7 @@ class Human(object):
     def get_action(self, board):
         move = int(input('Your move: '))
         return move, None
-    
+
     def __str__(self):
         return "Human {}".format(self.player)
 
@@ -41,26 +32,14 @@ def run():
     try:
         env = Env()
         game = Game(env)
-
-        # ############### human VS AI ###################
-        # load the trained policy_value_net in either Theano/Lasagne, PyTorch or TensorFlow
-
-        # best_policy = PolicyValueNet(width, height, model_file = model_file)
-        # mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=400)
-
-        # load the provided model (trained in Theano/Lasagne) into a MCTS player written in pure numpy
         policy_value_net = PolicyValueNet(
             0, params, 'cuda')
         az_player = MCTSPlayer(policy_value_net.policy_value_fn, c_puct=5,
-                                         n_playout=500, is_selfplay=0)  # set larger n_playout for better performance
+                               n_playout=500, is_selfplay=0)
         az_player.eval()
-        # uncomment the following line to play with pure MCTS (it's much weaker even with a larger n_playout)
         mcts_player = MCTS_Pure(c_puct=5, n_playout=500)
-
-        # human player, input your move in the format: 2,3
         # human = Human()
 
-        # set start_player=0 for human first
         game.start_play(az_player, mcts_player, show=1)
         # game.start_play(mcts_player, az_player, show=1)
     except KeyboardInterrupt:
