51d3c74590d95608ddfddae2d6b7c6241ac54d40
Sunshine <twq566256@foxmail.com>
2024-07-22 22:38:14 +1000

    chore: Add command line arguments for playing as X or O in human_play.py

--- human_play.py
+++ human_play.py
@@ -7,7 +7,14 @@ from env import Env, Game
 from MCTS import MCTSPlayer as MCTS_Pure
 from MCTS_AZ import AlphaZeroPlayer as MCTSPlayer
 from Network import PolicyValueNet
+import argparse
 
+parser = argparse.ArgumentParser(description='Play connect four with AlphaZero!')
+parser.add_argument('-x', action='store_true', help='Play as X')
+parser.add_argument('-o', action='store_true', help='Play as O')
+parser.add_argument('-s', type=int, default=1000, help='Number of simulations before AlphaZero make an action')
+
+args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -38,12 +45,19 @@ def run():
         game = Game(env)
         policy_value_net = PolicyValueNet(0, params, device)
         az_player = MCTSPlayer(policy_value_net.policy_value_fn, c_puct=5,
-                               n_playout=100, is_selfplay=0)
+                               n_playout=args.s, is_selfplay=0)
         az_player.eval()
-        mcts_player = MCTS_Pure(c_puct=5, n_playout=370)
+        # mcts_player = MCTS_Pure(c_puct=5, n_playout=370)
         human = Human()
-
-        game.start_play(human, mcts_player, show=1)
+        if args.x and args.o:
+            game.start_play(human, human, show=1)
+        elif args.x:
+            game.start_play(human, az_player, show=1)
+        elif args.o:
+            game.start_play(az_player, human, show=1)
+        else:
+            raise ValueError('You must choose to play as either X or O, or both X and O\n'
+                             f"<usage> python3 human_play.py -x [-o] [-s 100]")
         # game.start_play(az_player, mcts_player, show=1)
         # game.start_play(mcts_player, az_player, show=1)
     except KeyboardInterrupt:
