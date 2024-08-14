5872e799e5d015f9986e8833fd294d0009f13dbc
Sunshine <twq566256@foxmail.com>
2024-08-14 20:59:33 +1000

    Fix bug

--- env.py
+++ env.py
@@ -106,8 +106,7 @@ class Game:
                             pow(discount, len(winner_z) - idx - 1)
                 if show:
                     if winner != 0:
-                        print(f"Game end. Wineer is Player: {
-                              [None, 'X', 'O'][int(winner)]}")
+                        print(f"Game end. Wineer is Player: {[None, 'X', 'O'][int(winner)]}")
                     else:
                         print('Game end. Draw')
                 return winner, zip(states, mcts_probs, winner_z)
