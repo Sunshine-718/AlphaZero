1b61e34202d14c6fb3b1e322f319417806388633
Sunshine <twq566256@foxmail.com>
2025-04-04 19:12:16 +1100

    Fix bug

--- environments/Connect4/config.py
+++ environments/Connect4/config.py
@@ -12,7 +12,7 @@ training_config = {
     "pure_mcts_n_playout": 1000,
     "dirichlet_alpha": 0.5,   # depends on the size of action space
     "init_elo": 1500,
-    "num_eval": 10,
+    "num_eval": 50,
     "win_rate_threshold": 0.6,
 }
 
